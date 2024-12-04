135baac4e19e4eae9f201b1cfab19c49a32bc151
Ronnie Woo <rwoo50@stuy.edu>
2024-12-04 14:46:21 -0500

    saved work

--- parse.c
+++ parse.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include "parse.h"
 
+//takes a command line and an array of char *, and assigns each pointer in the char * array to each argument in the command, and sets the final pointer to point to NULL
+
 void parse_args( char * line, char ** arg_ary ){
     int i = 0;
     while (strchr(line, ' ') != NULL){
