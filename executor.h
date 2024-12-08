19ca87b42ac2a13e54c626cc0478c45e3e58641f
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2024-12-08 16:23:15 -0500

    function desc for parser and executor

--- executor.h
+++ executor.h
@@ -10,7 +10,34 @@
 #define AND_ACTIVE 2
 #define OR_ACTIVE 3
 
+/* execute_command
+ * 	executes a command
+ *
+ * ARGS
+ * 	cmd - the command to execute
+ * 	cmd_stack - if in the context of a command stack, the pointer to
+ * 		    the command stack being used
+ *	status - an array of integers denoting the status of certain intercommand conditions
+ *	in - a file number of stdin (or whatever input you use)
+ *	out - a file number of stdout (or whatever output you use)
+ *
+ * RETURN VALUE
+ * 	1 (true) if command executed successfully
+ * 	0 (false) if an error occured
+ */	
 int execute_command(struct command cmd, struct command_stack* cmd_stack, int* status, int in, int out);
+
+/* execute_command_stack
+ * 	executes commands from a command stack by popping
+ * 	and running a command until the stack is empty
+ *
+ * ARGS
+ * 	stack - a pointer to the command stack being used
+ *
+ * RETURN VALUE
+ * 	1 (true) if the command stack was executed successfully
+ * 	0 (false) if the command stack did not execute successfully
+ */
 int execute_command_stack(struct command_stack*);
 
 #endif
--- parse.h
+++ parse.h
@@ -5,10 +5,43 @@
 
 #define MAX_DELIM_L 5
 
-#define PARSE_ERR_ILLEGAL 2
-
+/* parse_input
+ * 	Parses a line of user input into an array of commands
+ *
+ * ARGS:
+ * 	out - an array of commands 
+ * 	line - the raw input from a user
+ *
+ * RETURN VALUE:
+ * 	returns a 1 if successfully parsed, a 0 if failed
+ */
 int parse_input(struct command* out, char* line);
+
+/* count_str_instances
+ * 	counts the amount of times a string (delim) 
+ * 	occurs in another string (str)
+ *
+ * ARGS:
+ * 	str - the string to search in
+ * 	delim - the string to look for
+ *
+ * RETURN VALUE:
+ * 	the amount of occurances of (delim) in (str)
+ */
 int count_str_instances(char* str, char* delim);
+
+/* str_in_arr
+ * 	returns 1 (true) if a string (str) is contained
+ * 	in an array of strings (arr).
+ *
+ * ARGS:
+ * 	str - the string you want to check
+ * 	arr - the array of strings
+ *
+ * RETURN VALUE:
+ * 	1 (true) if (str) does appear in (arr)
+ * 	0 (false) if (str) does not appear in (arr)
+ */
 int str_in_arr(char* str, char** arr, int n);
 
 #endif
