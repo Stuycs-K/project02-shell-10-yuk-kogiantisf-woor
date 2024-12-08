a950fc9796862f8e6a3acf91181da5452eedc518
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2024-12-08 18:45:39 -0500

    update main:

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "parse.h"
 #include "processor.h"
 #include "executor.h"
 
 #define INPUT_BUFFER_SIZE 1024 
-#define PATH_BUFFER_SIZE 256
+#define PATH_BUFFER_SIZE 1024
 
 #define STACK_SIZE 256
 
@@ -24,34 +25,40 @@ void print_prompt() {
 	fflush(stdout);
 }
 
-int main(){
-    //main loop
-    struct command_stack stack = init_stack(STACK_SIZE);
-    struct command parsed_commands[STACK_SIZE];
-    printf("Welcome to \033[33mPEE\033[1;0m (Practical Execution Environment) Shell!\n");
-    printf("Type \033[33mhelp\033[1;0m in the prompt if you need help!\n");
+int main() {
+	int first_run = 1;
+	struct command_stack stack = init_stack(STACK_SIZE);
+	struct command parsed_commands[STACK_SIZE];
+	while (1) {
+		if (first_run) {
+			printf("Welcome to \033[33mPEE\033[1;0m (Practical Execution Environment) Shell!\n");
+			printf("Type \033[33mhelp\033[1;0m in the prompt if you need help!\n");
+			first_run = 0;
+		}
 
-    while (1){
-        char line[INPUT_BUFFER_SIZE];
+		char line[INPUT_BUFFER_SIZE];
 
-        //Print the user prompt
-        print_prompt();
+		//Print the user prompt
+		print_prompt();
 
-        //read in user input
-        fgets(line, INPUT_BUFFER_SIZE, stdin);
+		//read in user input
+		if (fgets(line, INPUT_BUFFER_SIZE, stdin) == NULL) {
+			return 0;
+		}
 
-        //remove newline at the end
-        if (strchr(line, '\n') != NULL){
-            *strchr(line, '\n') = '\0';
-        }
-        
-        //parse user input
-        parse_input(parsed_commands, line);
+		//remove newline at the end
+		if (strchr(line, '\n') != NULL){
+			*strchr(line, '\n') = '\0';
+		}
 
-        //feed that user input onto the stack
-        process_commands_to_stack(&stack, parsed_commands);
+		//parse user input
+		parse_input(parsed_commands, line);
 
-        //execute commands
-        execute_command_stack(&stack);
-    }
+		//feed that user input onto the stack
+		process_commands_to_stack(&stack, parsed_commands);
+
+		//execute commands
+		execute_command_stack(&stack);
+	}
+
 }
